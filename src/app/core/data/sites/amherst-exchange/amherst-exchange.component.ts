652fc3f6931573c2246e590dfd1bb7141df838a6
Greg Boydston <gregboydston@mac.mynetworksettings.com>
2026-09-10 15:48:44 -0400

    4. Fix Exchange active tab 5. Fix Exchange search-bar layout 6. Remove Exchange Login 7. Remove Exchange Post Item 8. Remove dead Exchange footer links 9. Make all Exchange listings clickable/detail pages

--- src/app/core/data/sites/amherst-exchange/amherst-exchange.component.ts
+++ src/app/core/data/sites/amherst-exchange/amherst-exchange.component.ts
@@ -47,4 +47,48 @@ export class AmherstExchangeComponent {
     return this.page.type === 'EXCHANGE_LISTING';
   }
 
+
+  isCategoryActive(
+    categoryPath: string
+  ): boolean {
+
+    if (this.page.path === categoryPath) {
+      return true;
+    }
+
+
+    if (
+      categoryPath === '/category/vehicles' &&
+      this.isListing() &&
+      this.listing?.category === 'Vehicles'
+    ) {
+      return true;
+    }
+
+
+    return false;
+  }
+
+    get listingBackPath(): string {
+
+        return '/';
+        // TODO: update below switch to check for path
+            // that user entered listing from
+            // if entered from ALL return to '/'
+            // else return to the category user came from
+
+        // switch (this.listing?.category) {
+        // case 'Vehicles':
+        //     return '/category/vehicles';
+
+        // case 'Furniture':
+        //     return '/category/furniture';
+
+        // case 'Electronics':
+        //     return '/category/electronics';
+
+        // default:
+        //     return '/category/misc';
+        // }
+    }
 }
