4a2660a9d1c967186586bbb369618bac2e1aaa0c
Greg Boydston <gregboydston@mac.mynetworksettings.com>
2026-09-22 12:50:38 -0400

    create notification system

--- src/app/layout/app-shell/app-shell.ts
+++ src/app/layout/app-shell/app-shell.ts
@@ -2,13 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from '../../shared/navbar/navbar';
 import { SidebarComponent } from '../../shared/sidebar/sidebar';
+import {
+  ToastNotificationComponent
+} from '../../shared/toast-notification/toast-notification';
 
 @Component({
   selector: 'app-app-shell',
   imports: [
     RouterOutlet,
     NavbarComponent,
-    SidebarComponent
+    SidebarComponent,
+    ToastNotificationComponent
   ],
   templateUrl: './app-shell.html',
   styleUrl: './app-shell.scss'
